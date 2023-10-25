e03068a8c4809c46dd5c9bd71f06207289f953f3
yannis bouhamou <yannis.bouhamou@hotmail.fr>
2023-10-25 19:41:40 +0200

    ok

--- opt/airflow/dags/dag.py
+++ opt/airflow/dags/dag.py
@@ -11,9 +11,28 @@ with DAG('postgres_dag',
          default_args=default_args, 
          schedule_interval='*/1 * * * *',  # This schedules the DAG to run every minute
          catchup=False) as dag:
+    
+    
 
-    run_sql = PostgresOperator(
-        task_id='run_sql',
-        postgres_conn_id='postgres',  # Use the connection ID you created
-        sql='SELECT * FROM races;',
+    create_table = PostgresOperator(
+        task_id='create_table',
+        sql='''CREATE TABLE new_table(
+            custom_id integer NOT NULL, timestamp TIMESTAMP NOT NULL, user_id VARCHAR (50) NOT NULL
+            );''',
     )
+    
+    insert_row = PostgresOperator(
+    task_id='insert_row',
+    sql='INSERT INTO new_table VALUES(%s, %s, %s)',
+    trigger_rule=TriggerRule.ALL_DONE,
+    parameters=(uuid.uuid4().int % 123456789, datetime.now(), uuid.uuid4().hex[:10])
+)
+
+    create_table >> insert_row
+
+
+    # run_sql = PostgresOperator(
+    #     task_id='run_sql',
+    #     postgres_conn_id='postgres',  # Use the connection ID you created
+    #     sql='SELECT * FROM races;',
+    # )
