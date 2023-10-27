c58fa16eadeb5ae9eff3d61230da659f27c654b0
yannis bouhamou <yannis.bouhamou@hotmail.fr>
2023-10-27 14:50:58 +0200

    ok

--- opt/airflow/dags/dag.py
+++ opt/airflow/dags/dag.py
@@ -1,40 +1,52 @@
-from datetime import datetime
 from airflow import DAG
-import uuid
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.utils.trigger_rule import TriggerRule
+from airflow.operators.postgres_operator import PostgresOperator
+from datetime import datetime
+
 
 default_args = {
-    'owner': 'me',
-    'start_date': datetime(2023, 10, 25),
+    'owner': 'airflow',
+    'start_date': datetime(2023, 10, 27),
+    'retries': 1,
 }
 
-with DAG('postgres_dag', 
-         default_args=default_args, 
-         schedule_interval='*/1 * * * *',  # This schedules the DAG to run every minute
-         catchup=False) as dag:
-    
-    
-
-    create_table = PostgresOperator(
-        task_id='create_table',
-        sql='''CREATE TABLE new_table(
-            custom_id integer NOT NULL, timestamp TIMESTAMP NOT NULL, user_id VARCHAR (50) NOT NULL
-            );''',
-    )
-    
-    insert_row = PostgresOperator(
-    task_id='insert_row',
-    sql='INSERT INTO new_table VALUES(%s, %s, %s)',
-    trigger_rule=TriggerRule.ALL_DONE,
-    parameters=(uuid.uuid4().int % 123456789, datetime.now(), uuid.uuid4().hex[:10])
+dag = DAG(
+    'postgres_test',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval='@daily',
 )
 
-    create_table >> insert_row
+# Create Table Task
+create_table = PostgresOperator(
+    task_id='create_table',
+    postgres_conn_id='postgres',  # replace with your connection id
+    sql="""
+        CREATE TABLE IF NOT EXISTS test_table (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255)
+        );
+    """,
+    dag=dag,
+)
 
+# Insert Data Task
+insert_data = PostgresOperator(
+    task_id='insert_data',
+    postgres_conn_id='postgres',  # replace with your connection id
+    sql="""
+        INSERT INTO test_table (name)
+        VALUES ('John Doe'), ('Jane Doe');
+    """,
+    dag=dag,
+)
 
-    # run_sql = PostgresOperator(
-    #     task_id='run_sql',
-    #     postgres_conn_id='postgres',  # Use the connection ID you created
-    #     sql='SELECT * FROM races;',
-    # )
+# Select Data Task
+select_data = PostgresOperator(
+    task_id='select_data',
+    postgres_conn_id='postgres',  # replace with your connection id
+    sql="SELECT * FROM test_table;",
+    dag=dag,
+)
+
+# Set Task Dependencies
+create_table >> insert_data >> select_data
